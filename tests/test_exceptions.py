42f02064535e2a7b54263af61fbd3cd587e7efee
cheginit <cheginit@gmail.com>
2021-05-26 21:43:47 -0500

    ENH: Add test for the new exception

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -37,3 +37,12 @@ def invalid_type():
 def test_invalid_type():
     with pytest.raises(InvalidInputType):
         invalid_type()
+
+
+def invalid_range():
+    raise InvalidInputRange("Input is out of range.")
+
+
+def test_invalid_range():
+    with pytest.raises(InvalidInputRange):
+        invalid_range()
