92b59426204b8e447187260d1e7d5bed7f12f7fb
cheginit <cheginit@gmail.com>
2021-07-13 13:31:25 -0500

    ENH: Add async_retriever

--- noxfile.py
+++ noxfile.py
@@ -4,7 +4,7 @@ import nox
 @nox.session(python="3.9")
 def tests(session):
     session.install(".[test]")
-    hr_deps = ["pygeoogc", "pygeoutils"]
+    hr_deps = ["async_retriever", "pygeoogc", "pygeoutils"]
     for p in hr_deps:
         session.install(f"git+https://github.com/cheginit/{p}.git")
     session.run("pytest")
