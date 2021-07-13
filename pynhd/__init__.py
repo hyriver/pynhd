8eae6ab12b15083bf73ea9878262235c39c177d1
cheginit <cheginit@gmail.com>
2021-07-13 13:31:35 -0500

    ENH: Explicitly add all public functions to __all__

--- pynhd/__init__.py
+++ pynhd/__init__.py
@@ -1,7 +1,7 @@
 """Top-level package for PyNHD."""
 from pkg_resources import DistributionNotFound, get_distribution
 
-from .exceptions import InvalidInputRange, MissingItems, ZeroMatched
+from .exceptions import InvalidInputRange, MissingItems
 from .network_tools import prepare_nhdplus, topoogical_sort, vector_accumulation
 from .print_versions import show_versions
 from .pynhd import NLDI, AGRBase, NHDPlusHR, WaterData, nhd_fcode, nhdplus_attrs, nhdplus_vaa
@@ -9,5 +9,20 @@ from .pynhd import NLDI, AGRBase, NHDPlusHR, WaterData, nhd_fcode, nhdplus_attrs
 try:
     __version__ = get_distribution(__name__).version
 except DistributionNotFound:
-    # package is not installed
-    pass
+    __version__ = "999"
+
+__all__ = [
+    "InvalidInputRange",
+    "MissingItems",
+    "prepare_nhdplus",
+    "topoogical_sort",
+    "vector_accumulation",
+    "show_versions",
+    "NLDI",
+    "AGRBase",
+    "NHDPlusHR",
+    "WaterData",
+    "nhd_fcode",
+    "nhdplus_attrs",
+    "nhdplus_vaa",
+]
