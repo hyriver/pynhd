a25e5a81d2290fad72a12d984e2dbb3c0084eb22
cheginit <cheginit@gmail.com>
2022-04-26 21:42:04 -0500

    ENH: Add tests for the new geometry query field of geoconnex [skip ci]

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,12 +1,14 @@
 import pytest
 
-from pynhd import MissingItems
+import pynhd
+from pynhd import InvalidInputRange
 
 
-def missing_items():
-    raise MissingItems(["tmin", "dayl"])
-
-
-def test_missing_items():
-    with pytest.raises(MissingItems):
-        missing_items()
+class TestGCXException:
+    def test_wrong_bounds(self):
+        with pytest.raises(InvalidInputRange) as ex:
+            _ = pynhd.geoconnex(
+                item="gages",
+                query={"geometry": (1350626.862, 1687621.573, 1996597.949, 2557120.139)},
+            )
+        assert "(-170, 15, -51, 72)" in str(ex.value)
