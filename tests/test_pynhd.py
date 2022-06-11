386fe71a0dd6c748348873adf3863c076d2b5f09
cheginit <cheginit@gmail.com>
2022-06-10 22:27:42 -0500

    ENH: Change the test name to make sure all basin related test have similar names

--- tests/test_pynhd.py
+++ tests/test_pynhd.py
@@ -152,7 +152,7 @@ class TestNLDI:
         split = self.nldi.get_basins(STA_ID, split_catchment=True).to_crs(eck4)
         assert abs((split.area.values[0] - basin.area.values[0]) - 0.489) < SMALL
 
-    def test_empty_basin(self):
+    def test_basin_empty(self):
         empty_ids = ["04253294", "04253296"]
         _, not_found = self.nldi.get_basins(empty_ids)
         assert not_found == empty_ids
